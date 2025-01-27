b842ab59a8cf769b059fa867278fb46420ea0e98
Paulo Miguel <paulo2.0miguel@gmail.com>
2025-01-27 20:26:23 -0300

    feito a interface

---
+++ src/App.js
@@ -0,0 +1,45 @@
+import React, { useContext } from "react";
+import Inteface from "./Interface";
+import { GenericDates,GenericDatesProvider } from "./GenericsDate";
+
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend,
+} from 'chart.js';
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend
+);
+
+
+
+function App() {
+
+  return (
+    <GenericDatesProvider>
+      <Inteface />
+    </GenericDatesProvider>
+
+  );
+}
+
+export default App;
+
+
+
+
+
+
+
+
